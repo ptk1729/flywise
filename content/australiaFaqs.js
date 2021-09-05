8222e79dc424f8c83164b9382a653d01d3b0f1d2
tanisha.uni <jaintanisha52@gmail.com>
2021-09-06 01:48:18 +0530

    created separate folder for content

---
+++ content/australiaFaqs.js
@@ -0,0 +1,50 @@
+let faqs = [
+  {
+    ques: "Can students work during their studies in Australia?",
+    ans: [
+      `Ofcourse, working as a student will help to gain extra money to pay your own bills and some companies provide flexible job hours for students like starbucks. `,
+    ],
+  },
+
+  {
+    ques: "Which English language tests are accepted by Australian institutions?",
+    ans: [
+      `Few English language tests acceptable in Australia are :
+  International English Language Testing System (IELTS)
+  Test of English as a Foreign Language (TOEFL) internet-based test (paper-based test is accepted from limited countries)
+  Cambridge English: Advanced (CAE)
+  Pearson Test of English (PTE) Academic.
+  `,
+    ],
+  },
+
+  {
+    ques: "What are the accommodation options available in Australia? ",
+    ans: [
+      `There are four main types of student accommodation in Australia: purpose-built student housing, homestay, private rentals and residential colleges. Some universities may offer some different options, but they will usually fall under these categories in some way.`,
+    ],
+  },
+
+  {
+    ques: "what are some interesting facts about Australian universities?",
+    ans: [
+      `Australia has an outstanding higher education system. With over 22,000 courses across 1,100 institutions, our system is ranked 8th in the Universitas 2019 U21 Ranking of National Higher Education Systems, higher than France, Germany, Norway and Japan.`,
+    ],
+  },
+
+  {
+    ques: "Can students work during studies in Australia? ",
+    ans: [
+      `Yes, if a student is not able to visit physically then he/ she can apply for online courses.`,
+    ],
+  },
+
+  {
+    ques: "What all things i should carry with me according to climate conditions there?",
+    ans: [
+      `Australia has a more tropical influenced climate, hot and humid in the summer, and quite warm and dry in the winter, while the southern parts are cooler with mild summers and cool, sometimes rainy winters. Its better to carry your stuff according to climate conditions.`,
+    ],
+  },
+];
+
+export default faqs;
---
+++ content/homeFaqs.js
@@ -0,0 +1,53 @@
+let faqs = [
+  {
+    ques: "Why FlyWise?",
+    ans: [
+      "Flywise is a systematic guide, tailored for you right from the initial thought of pursuing Masters' to landing your dream tech job in the USA.",
+      "Worried how you could land a tech job without a tech degree ? Worried about expenses ? Issues about convincing family? We got it all!",
+      "Our motto is transparency in support and building your future!" +
+        "Our mission is to be with you until you land your dream job. (Currently, all the study abroad education consultants only helps to get an admit)",
+    ],
+  },
+  {
+    ques: "Is Flywise completely virtual/online?",
+    ans: [
+      "Our founders and mentors live abroad.",
+      "Benefits of Online Counseling:",
+      "			A. Saves travelling time and money",
+      "			B. Dedicated support available just in a whatsapp message distance 24/7",
+      "			C. It’s convenient because you can track everything just on your mobile phone.",
+    ],
+  },
+  {
+    ques: "Does Flywise provide loan/funding support?",
+    ans: [
+      "YES, we work with major loan providers (collateral & non-collateral) in the market to help  you get the best interest rate.",
+    ],
+  },
+  {
+    ques: "",
+    ans: [],
+  },
+  {
+    ques: "",
+    ans: [],
+  },
+  {
+    ques: "",
+    ans: [],
+  },
+  {
+    ques: "",
+    ans: [],
+  },
+  {
+    ques: "",
+    ans: [],
+  },
+  {
+    ques: "",
+    ans: [],
+  },
+];
+
+export default faqs;
