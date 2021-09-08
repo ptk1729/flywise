a622d858961bf8d6cf4fad03cfbad2bbfd6d82e4
harshit0606 <harshitasr001@gmail.com>
2021-09-08 17:24:24 +0530

    floating button added

---
+++ content/parent.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import Image from "next/image";
+import Whats from "../public/images/icons/whattt.png";
+import classes from "../styles/whatsappbtn.module.css";
+import { useRouter } from "next/router";
+
+const Parentdiv = ({ children }) => {
+  const path = useRouter().pathname;
+  console.log(path)
+  return (
+    <div>
+      <Fragment>{children}</Fragment>
+      {path !== "/profile-evaluation" ? (
+        <div className={classes.what}>
+          <Image src={Whats} />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Parentdiv;
